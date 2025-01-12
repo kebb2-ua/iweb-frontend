6fd7bfa2a32994a43610b85dc9939d0a4d3d9a31
Kyllian Edouard Boucir <kebb2@alu.ua.es>
2025-01-12 01:16:19 +0100

    Layout de la app

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,4 +1,29 @@
 <template>
+  <v-navigation-drawer
+    expand-on-hover
+    rail
+  >
+    <v-list>
+      <v-list-item
+        prepend-icon="mdi-account-circle"
+        subtitle="Sesión no iniciada"
+        title="Anónimo"
+      />
+    </v-list>
+
+      <v-divider />
+
+    <v-list>
+      <v-list-item
+        v-for="item in menu"
+        :key="item.text"
+        :prepend-icon="item.icon"
+        :title="item.text"
+        link
+      />
+    </v-list>
+  </v-navigation-drawer>
+
   <v-main>
     <router-view />
   </v-main>
@@ -7,5 +32,13 @@
 </template>
 
 <script setup>
-  //
+const menu = [
+  { icon: 'mdi-home', text: 'Inicio' },
+  { icon: 'mdi-account', text: 'Catálogo' },
+  { icon: 'mdi-magnify', text: 'Buscar' },
+  { icon: 'mdi-account-group', text: 'Sobre nosotros' },
+  { icon: 'mdi-cart', text: 'Cesta' },
+  { icon: 'mdi-login-variant', text: 'Iniciar sesión' },
+  { icon: 'mdi-logout', text: 'Registro' },
+]
 </script>
