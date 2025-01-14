7de0a191a2d925eb32159a35ef204d109e85557a
Kyllian Edouard Boucir <kebb2@alu.ua.es>
2025-01-14 18:34:22 +0100

    Menú con división

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -15,14 +15,20 @@
 
     <v-divider />
     <v-list>
-      <v-list-item
+      <template
         v-for="item in menu"
         :key="item.text"
-        :prepend-icon="item.icon"
-        :title="item.text"
-        :to="item.path"
-        link
-      />
+      >
+        <v-divider v-if="item.type == 'divider'" />
+        
+        <v-list-item
+          v-else
+          :prepend-icon="item.icon"
+          :title="item.text"
+          :to="item.path"
+          link
+        />
+      </template>
     </v-list>
   </v-navigation-drawer>
 
@@ -47,6 +53,7 @@ const menuItems = reactive([
   { icon: 'mdi-account', text: 'Perfil', path: '/perfil', show: computed(() => authStore.isAuthenticated) },
   { icon: 'mdi-cog', text: 'Configuración', path: '/perfil/editar', show: computed(() => authStore.isAuthenticated) },
   { icon: 'mdi-logout', text: 'Cerrar sesión', path: '/logout', show: computed(() => authStore.isAuthenticated) },
+  { type: 'divider' },
   { icon: 'mdi-account-group', text: 'Lista de usuarios', path: '/admin/listausuarios', show: computed(() => authStore.isAuthenticated),},
   { icon: 'mdi-truck-delivery', text: 'Lista de envíos', path: '/admin/listaenvios', show: computed(() => authStore.isAuthenticated), },
 ])
