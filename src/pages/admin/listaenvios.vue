0b3cb580a0d3796a7eee65a1f25f92a2e0063ef7
samuel2793 <padillasamuel2793@gmail.com>
2025-01-14 16:39:45 +0100

    lista de envios

---
+++ src/pages/admin/listaenvios.vue
@@ -0,0 +1,34 @@
+<route lang="json">
+  {
+    "name": "envios",
+    "meta": {
+      "requiresAuth": true
+    }
+  }
+  </route>
+
+  <script setup>
+  import EnviosList from '@/components/EnviosList.vue';
+  </script>
+
+<template>
+  <v-container>
+    <v-img
+      class="mx-auto my-6"
+      max-width="128"
+      src="@/assets/logo.svg"
+    />
+
+    <v-card
+      class="mx-auto pa-8"
+      elevation="8"
+      max-width="1024"
+      rounded="lg"
+    >
+      <v-card-title>Lista de Envíos</v-card-title>
+      <v-card-text>
+        <EnviosList />
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
