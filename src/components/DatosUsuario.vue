82bb6c14c3574afb4161ff65a43d7704f2c9cea4
Kyllian Edouard Boucir <kebb2@alu.ua.es>
2025-01-15 17:58:42 +0100

    Ajustes hecho en componentes

---
+++ src/components/DatosUsuario.vue
@@ -0,0 +1,37 @@
+<script setup>
+defineProps({
+  user: {
+    type: Object,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <v-card>
+    <v-card-title>
+      <h3>Hola, {{ user.nombre }}</h3>
+    </v-card-title>
+    <v-card-text>
+      <p>Tus datos personales registrados son:</p>
+      <v-list>
+        <v-list-item>
+          <v-list-item-title>Nombre</v-list-item-title>
+          <v-list-item-subtitle>{{ user.nombre }}</v-list-item-subtitle>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-title>Apellidos</v-list-item-title>
+          <v-list-item-subtitle>{{ user.apellidos }}</v-list-item-subtitle>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-title>Número de Identificación Fiscal (NIF)</v-list-item-title>
+          <v-list-item-subtitle>{{ user.nif }}</v-list-item-subtitle>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-title>Correo electrónico</v-list-item-title>
+          <v-list-item-subtitle>{{ user.email }}</v-list-item-subtitle>
+        </v-list-item>
+      </v-list>
+    </v-card-text>
+  </v-card>
+</template>
--- src/pages/ajustes.vue
+++ src/pages/ajustes.vue
@@ -8,7 +8,8 @@
 </route>
 
 <script setup>
-import NuevaClaveAPI from '@/components/NuevaClaveAPI.vue';
+import DatosUsuario from '@/components/DatosUsuario.vue';
+import ListadoApiKeys from '@/components/ListadoApiKeys.vue';
 import { useApiKeyStore } from '@/stores/apikeys';
 import { useAuthStore } from '@/stores/auth';
 import { storeToRefs } from 'pinia';
@@ -33,72 +34,14 @@ onMounted(async () => {
         cols="12"
         md="6"
       >
-        <v-card>
-          <v-card-title>
-            <h3>Hola, {{ authStore.user.nombre }}</h3>
-          </v-card-title>
-          <v-card-text>
-            <p>Tus datos personales registrados son:</p>
-            <v-list>
-              <v-list-item>
-                <v-list-item-title>Nombre</v-list-item-title>
-                <v-list-item-subtitle>{{ authStore.user.nombre }}</v-list-item-subtitle>
-              </v-list-item>
-              <v-list-item>
-                <v-list-item-title>Apellidos</v-list-item-title>
-                <v-list-item-subtitle>{{ authStore.user.apellidos }}</v-list-item-subtitle>
-              </v-list-item>
-              <v-list-item>
-                <v-list-item-title>Número de Identificación Fiscal (NIF)</v-list-item-title>
-                <v-list-item-subtitle>{{ authStore.user.nif }}</v-list-item-subtitle>
-              </v-list-item>
-              <v-list-item>
-                <v-list-item-title>Correo electrónico</v-list-item-title>
-                <v-list-item-subtitle>{{ authStore.user.email }}</v-list-item-subtitle>
-              </v-list-item>
-            </v-list>
-          </v-card-text>
-        </v-card>
+        <DatosUsuario :user="authStore.user" />
       </v-col>
 
       <v-col
         cols="12"
         md="6"
       >
-        <v-card>
-          <v-card-title>
-            <h3>Tus claves API</h3>
-          </v-card-title>
-          <v-card-text>
-            <p>Estas son las claves API que has generado:</p>
-          </v-card-text>
-          <v-list density="compact">
-            <v-list-item v-if="keys.length === 0">
-              No has generado ninguna clave API.
-            </v-list-item>
-
-            <v-list-item
-              v-for="apiKey in keys"
-              :key="apiKey.id"
-              :title="apiKey.publicId"
-            >
-              <template #append>
-                <v-list-item-action>
-                  <v-btn
-                    prepend-icon="mdi-trash-can"
-                    color="error"
-                    @click="apiKeyStore.deleteApiKey(apiKey.publicId)"
-                  >
-                    Eliminar
-                  </v-btn>
-                </v-list-item-action>
-              </template>
-            </v-list-item>
-          </v-list>
-          <v-card-actions>
-            <NuevaClaveAPI />
-          </v-card-actions>
-        </v-card>
+        <ListadoApiKeys :keys="keys" />
       </v-col>
     </v-row>
   </v-container>
