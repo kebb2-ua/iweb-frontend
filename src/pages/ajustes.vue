5f3782a27a16933f3d1829bbc12811d51df98455
Kyllian Edouard Boucir <kebb2@alu.ua.es>
2025-01-15 01:15:28 +0100

    Login ahora devuelve el objeto usuario

--- src/pages/ajustes.vue
+++ src/pages/ajustes.vue
@@ -1,5 +1,43 @@
+<script setup>
+import { useAuthStore } from '@/stores/auth';
+
+const authStore = useAuthStore();
+</script>
+
 <template>
   <v-container>
-    todo
+    <v-row>
+      <v-col cols="12" md="6">
+        <v-card>
+          <v-card-title>
+            <h3>Hola, {{ authStore.user.nombre }}</h3>
+          </v-card-title>
+          <v-card-text>
+            asdasd
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="primary">
+              Actualizar
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+
+      <v-col cols="12" md="6">
+        <v-card>
+          <v-card-title>
+            <h3>Tus claves API</h3>
+          </v-card-title>
+          <v-card-text>
+            asdasd
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="primary">
+              Actualizar
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
