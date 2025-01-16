4ea90a7ce026c6219c7b3a0b6b79c4fe7b2b680e
David Almenar Martínez <dam50@alu.ua.es>
2025-01-16 18:23:11 +0100

    Estructura inicial

---
+++ src/pages/reparto/pedido.vue
@@ -0,0 +1,34 @@
+<route lang="json">
+{
+  "name": "pedido-ruta",
+  "meta": {
+    "requiresAuth": true
+  }
+}
+</route>
+
+<script setup>
+import EnviosList from '@/components/PedidoRuta.vue';
+</script>
+
+<template>
+  <v-container>
+    <v-img
+      class="mx-auto my-6"
+      max-width="128"
+      src="@/assets/logo.svg"
+    />
+
+    <v-card
+      class="mx-auto pa-8"
+      elevation="8"
+      max-width="1024"
+      rounded="lg"
+    >
+      <v-card-title>Detalles del Envío</v-card-title>
+      <v-card-text>
+        <EnviosList />
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
---
+++ src/pages/reparto/ruta.vue
@@ -0,0 +1,34 @@
+<route lang="json">
+{
+  "name": "ruta",
+  "meta": {
+    "requiresAuth": true
+  }
+}
+</route>
+
+<script setup>
+import EnviosList from '@/components/RutaDiaria.vue';
+</script>
+
+<template>
+  <v-container>
+    <v-img
+      class="mx-auto my-6"
+      max-width="128"
+      src="@/assets/logo.svg"
+    />
+
+    <v-card
+      class="mx-auto pa-8"
+      elevation="8"
+      max-width="1024"
+      rounded="lg"
+    >
+      <v-card-title>Ruta de Reparto</v-card-title>
+      <v-card-text>
+        <EnviosList />
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
